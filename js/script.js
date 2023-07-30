c82925cf9e983789049eaf6a319362c085c96a9c
MLUSTADO <139263695+MLUSTADO@users.noreply.github.com>
2023-07-30 10:40:29 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,62 @@
-const hamburger = document.querySelector(".hamburger");
+// Function to handle form validation and display success message
+function validate() {
+    var fields = document.querySelectorAll('.form-container textarea, .form-container input[type="text"]');
+    var check = true;
+
+    for (var i = 0; i < fields.length; i++) {
+        if (fields[i].value.trim() === "") {
+            var errorSpan = fields[i].nextElementSibling;
+            if (!errorSpan) {
+                errorSpan = document.createElement("span");
+                errorSpan.classList.add("error");
+                fields[i].parentNode.appendChild(errorSpan);
+            }
+            errorSpan.innerHTML = "Oops! " + fields[i].placeholder + " is required.";
+            fields[i].style.boxShadow = "0 0 2px 1px #cc0001";
+            check = false;
+        } else {
+            var errorSpan = fields[i].nextElementSibling;
+            if (errorSpan) {
+                errorSpan.innerHTML = "";
+            }
+            fields[i].style.boxShadow = "none";
+        }
+    }
+
+    return check;
+}
+
+// Function to show the success message
+function showSuccessMessage() {
+    var successMessage = document.createElement("div");
+    successMessage.textContent = "Message has been sent. Expect a response within 24 hours.";
+    successMessage.classList.add("success-message");
+    document.body.appendChild(successMessage);
+
+    // Hide the success message after 5 seconds (adjust as needed)
+    setTimeout(function () {
+        document.body.removeChild(successMessage);
+    }, 5000);
+}
+
+// Function to clear the form fields after successful submission
+function clearForm(fields) {
+    for (var i = 0; i < fields.length; i++) {
+        fields[i].value = "";
+    }
+}
+
+function submitForm() {
+    if (validate()) {
+        showSuccessMessage();
+        var fields = document.querySelectorAll('.form-container textarea, .form-container input[type="text"]');
+        clearForm(fields);
+    }
+}
+
+
+  // Menu Logic
+  const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
   hamburger.addEventListener("click", () => {
